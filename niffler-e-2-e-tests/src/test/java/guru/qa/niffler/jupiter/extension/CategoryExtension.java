af42209af6ac5d8cff79719705cdfd5d6be16fc4
Roman Katkov <r.katkov@mellow.io>
2025-06-22 23:10:44 +0700

    CategoryExtension added

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -0,0 +1,72 @@
+package guru.qa.niffler.jupiter.extension;
+
+import com.github.javafaker.Faker;
+import guru.qa.niffler.api.SpendApiClient;
+import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.model.CategoryJson;
+import org.junit.jupiter.api.extension.*;
+import org.junit.platform.commons.support.AnnotationSupport;
+
+import java.util.UUID;
+
+public class CategoryExtension implements BeforeEachCallback, AfterTestExecutionCallback, ParameterResolver {
+
+    public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(CategoryExtension.class);
+    private final SpendApiClient spendApiClient = new SpendApiClient();
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        AnnotationSupport.findAnnotation(
+                context.getRequiredTestMethod(),
+                Category.class
+        ).ifPresent(
+                anno -> {
+                    CategoryJson categoryJson = new CategoryJson(
+                            UUID.randomUUID(),
+                            new Faker().funnyName().name(),
+                            anno.username(),
+                            false
+                    );
+                    CategoryJson created = spendApiClient.addCategory(categoryJson);
+                    if(anno.archived()){
+                        CategoryJson archivedCategory = new CategoryJson(
+                                created.id(),
+                                created.name(),
+                                created.username(),
+                                true
+                        );
+                        created = spendApiClient.editCategory(archivedCategory);
+                    }
+                    context.getStore(NAMESPACE).put(
+                            context.getUniqueId(),
+                            created
+                    );
+                }
+        );
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType().isAssignableFrom(CategoryJson.class);
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), CategoryJson.class);
+    }
+
+
+    @Override
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        CategoryJson category = context.getStore(NAMESPACE)
+                .get(context.getUniqueId(), CategoryJson.class);
+        if(!category.archived()){
+            CategoryJson archivedCategory = new CategoryJson(
+                    category.id(),
+                    category.name(),
+                    category.username(),
+                    true);
+            spendApiClient.editCategory(archivedCategory);
+        }
+    }
+}
