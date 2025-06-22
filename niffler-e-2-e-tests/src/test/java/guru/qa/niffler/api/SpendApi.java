ab63230b8afdec1b3bb775a34e6b084a63ef6084
Roman Katkov <r.katkov@mellow.io>
2025-06-22 17:20:09 +0700

    SpendApi class modified

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -1,12 +1,14 @@
 package guru.qa.niffler.api;
 
 import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.DeleteSpendingJson;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
 import retrofit2.http.*;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SpendApi {
 
@@ -20,10 +22,10 @@ public interface SpendApi {
   Call<SpendJson> getSpendById(@Path("id") int userId);
 
   @GET("internal/spends/all")
-  Call<List<SpendJson>> getSpendList();
+  Call<List<SpendJson>> getSpendList(@Query("filterCurrency")CurrencyValues currency);
 
   @DELETE("internal/spends/remove")
-  Call<DeleteSpendingJson> deleteSpend();
+  Call<DeleteSpendingJson> deleteSpend(@Query("ids") List<String> ids);
 
   @POST("internal/categories/add")
   Call<CategoryJson> addCategory(@Body CategoryJson category);
