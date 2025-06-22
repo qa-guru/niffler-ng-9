f3383cbad8706790229552372f1c44bdc9aec44f
Сычев Михаил Валерьевич <msychev@mts.ru>
2025-06-22 09:51:01 +0300

    add all controllers to SpendApi and add all spend api clients

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -1,12 +1,50 @@
 package guru.qa.niffler.api;
 
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
+
+import java.util.Date;
+import java.util.List;
 
 public interface SpendApi {
 
+  /**
+   * Spend Controller
+   */
+
   @POST("internal/spends/add")
   Call<SpendJson> addSpend(@Body SpendJson spending);
+
+  @PATCH("internal/spends/edit")
+  Call<SpendJson> editSpend(@Body SpendJson spending);
+
+  @GET("internal/spends/{id}")
+  Call<SpendJson> getSpend(@Path("id") String id, @Query("username") String username);
+
+  @GET("internal/spends/all")
+  Call<List<SpendJson>> getSpends(
+          @Query("username") String username,
+          @Query("filterCurrency") CurrencyValues filterCurrency,
+          @Query("from") Date from,
+          @Query("to") Date to
+  );
+
+  @DELETE("internal/spends/remove")
+  Call<Void> deleteSpends(@Query("username") String username, @Query("ids") List<String> ids);
+
+  /**
+   * Categories Controller
+   */
+
+  @GET("internal/categories/all")
+  Call<List<CategoryJson>> getCategories(@Query("username") String userName, @Query("excludeArchived") boolean excludeArchived);
+
+  @POST("internal/categories/add")
+  Call<CategoryJson> addCategory(@Body CategoryJson category);
+
+  @PATCH("internal/categories/update")
+  Call<CategoryJson> updateCategory(@Body CategoryJson category);
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -1,26 +1,118 @@
 package guru.qa.niffler.api;
 
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.http.Query;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 public class SpendApiClient {
 
-  private static final Config CFG = Config.getInstance();
+	private static final Config CFG = Config.getInstance();
 
-  private static final Retrofit retrofit = new Retrofit.Builder()
-      .baseUrl(CFG.spendUrl())
-      .addConverterFactory(JacksonConverterFactory.create())
-      .build();
+	private static final Retrofit retrofit = new Retrofit.Builder()
+			.baseUrl(CFG.spendUrl())
+			.addConverterFactory(JacksonConverterFactory.create())
+			.build();
 
-  private final SpendApi spendApi = retrofit.create(SpendApi.class);
+	private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  @SneakyThrows
-  public SpendJson addSpend(SpendJson spendJson) {
-    return spendApi.addSpend(spendJson)
-        .execute()
-        .body();
-  }
+	public SpendJson addSpend(SpendJson spendJson) {
+		final Response<SpendJson> response;
+		try {
+			response = spendApi.addSpend(spendJson).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(201, response.code());
+		return response.body();
+	}
+
+	public SpendJson editSpend(SpendJson spendJson) {
+		final Response<SpendJson> response;
+		try {
+			response = spendApi.editSpend(spendJson).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(200, response.code());
+		return response.body();
+	}
+
+	public SpendJson getSpend(String id, String username) {
+		final Response<SpendJson> response;
+		try {
+			response = spendApi.getSpend(id, username).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(200, response.code());
+		return response.body();
+	}
+
+	public List<SpendJson> getSpends(String username,
+	                                 CurrencyValues filterCurrency,
+	                                 Date from,
+	                                 Date to) {
+		final Response<List<SpendJson>> response;
+		try {
+			response = spendApi.getSpends(username, filterCurrency, from, to).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(200, response.code());
+		return response.body();
+	}
+
+	public void deleteSpends(String username, List<String> ids) {
+		final Response<Void> response;
+		try {
+			response = spendApi.deleteSpends(username, ids).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(202, response.code());
+	}
+
+	public List<CategoryJson> getCategories(String userName, boolean excludeArchived) {
+		final Response<List<CategoryJson>> response;
+		try {
+			response = spendApi.getCategories(userName, excludeArchived).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(200, response.code());
+		return response.body();
+	}
+
+	public CategoryJson addCategory(CategoryJson categoryJson) {
+		final Response<CategoryJson> response;
+		try {
+			response = spendApi.addCategory(categoryJson).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(200, response.code());
+		return response.body();
+	}
+
+	public CategoryJson updateCategory(CategoryJson categoryJson) {
+		final Response<CategoryJson> response;
+		try {
+			response = spendApi.updateCategory(categoryJson).execute();
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+		Assertions.assertEquals(200, response.code());
+		return response.body();
+	}
 }
