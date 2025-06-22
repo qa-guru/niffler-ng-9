1de4d77118b21ff2a539a12646e9d4053f6b9194
Roman Katkov <r.katkov@mellow.io>
2025-06-22 23:11:29 +0700

    New endpoints described in SpendApi class

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -2,30 +2,28 @@ package guru.qa.niffler.api;
 
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.CurrencyValues;
-import guru.qa.niffler.model.DeleteSpendingJson;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
 import retrofit2.http.*;
 
 import java.util.List;
-import java.util.Map;
 
 public interface SpendApi {
 
   @POST("internal/spends/add")
-  Call<SpendJson> addSpend(@Body SpendJson spending);
+  Call<SpendJson> addSpending(@Body SpendJson spending);
 
   @PATCH("internal/spends/edit")
-  Call<SpendJson> editSpend(@Body SpendJson spending);
+  Call<SpendJson> editSpending(@Body SpendJson spending);
 
   @GET("internal/spends/{id}")
-  Call<SpendJson> getSpendById(@Path("id") int userId);
+  Call<SpendJson> getSpendingById(@Path("id") String spendingId);
 
   @GET("internal/spends/all")
-  Call<List<SpendJson>> getSpendList(@Query("filterCurrency")CurrencyValues currency);
+  Call<List<SpendJson>> getSpendingList(@Query("filterCurrency")CurrencyValues currency);
 
   @DELETE("internal/spends/remove")
-  Call<DeleteSpendingJson> deleteSpend(@Query("ids") List<String> ids);
+  Call<Void> deleteSpending(@Query("ids") List<String> ids);
 
   @POST("internal/categories/add")
   Call<CategoryJson> addCategory(@Body CategoryJson category);
