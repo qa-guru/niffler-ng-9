21792a2f5e9cbde5577d388a7a76e87fbef9d3d7
Roman Katkov <r.katkov@mellow.io>
2025-06-22 15:58:09 +0700

    New login tests added to LoginTest class

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Selenide;
+import com.github.javafaker.Faker;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,36 @@ public class LoginTest {
 
   private static final Config CFG = Config.getInstance();
 
+  String username = new Faker().name().username();
+
   @Test
   void mainPageShouldBeDisplayedAfterSuccessLogin() {
     Selenide.open(CFG.frontUrl(), LoginPage.class)
         .fillLoginPage("duck", "12345")
         .submit()
         .checkThatPageLoaded();
+  }
+
+  @Test
+  void errorShouldBeShownIfUserNotRegistered(){
+    final String errorText = "Неверные учетные данные пользователя";
+
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+            .fillLoginPage(username, "12345")
+            .submitAndCheckErrorText(errorText);
+  }
+
+  //TODO registration of user before test
+  @Test
+  void errorShouldBeShownWithIncorrectRegisteredUserPassword(){
+
+  }
+
+  @Test
+  void passwordShouldBeShownAfterVisibilityChanging(){
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+            .fillLoginPage(username, "12345")
+            .showPassword()
+            .checkPasswordInputType();
   }
 }
