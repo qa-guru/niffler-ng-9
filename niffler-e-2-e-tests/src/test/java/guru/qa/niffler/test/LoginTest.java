6da167cf2b1b03ed3fa83e52206ad424824ad1bb
Сычев Михаил Валерьевич <msychev@mts.ru>
2025-06-22 21:51:01 +0300

    add practice from a lesson

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
@@ -2,10 +2,13 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.utils.Users;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(BrowserExtension.class)
 public class LoginTest {
 
   private static final Config CFG = Config.getInstance();
