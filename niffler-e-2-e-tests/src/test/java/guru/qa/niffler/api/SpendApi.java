dae4486666c958c8736ee81e31211f92ef4137e8
Nikita Kremnev <nikita.kremnev@altenar.com>
2025-06-22 13:36:49 +0300

    add spend api endpoints and client

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -1,12 +1,39 @@
 package guru.qa.niffler.api;
 
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface SpendApi {
 
   @POST("internal/spends/add")
-  Call<SpendJson> addSpend(@Body SpendJson spending);
+  Call<SpendJson> addSpend(@Body SpendJson spend);
+
+  @PATCH("internal/spends/edit")
+  Call<SpendJson> editSpend(@Body SpendJson spend);
+
+  @GET("internal/spends/{id}")
+  Call<SpendJson> getSpendById(@Path("id") String id);
+
+  @GET("internal/spends/all")
+  Call<List<SpendJson>> getAllSpends(
+          @Query("ids") List<String> ids,
+          @Query("filterCurrency") CurrencyValues currencyFilter
+  );
+
+  @DELETE("internal/spends/remove/{id}")
+  Call<Void> removeSpend(@Path("id") String id);
+
+  @POST("internal/categories/add")
+  Call<CategoryJson> addCategory(@Body CategoryJson category);
+
+  @PATCH("internal/categories/update")
+  Call<CategoryJson> updateCategory(@Body CategoryJson category);
+
+  @GET("internal/categories/all")
+  Call<List<CategoryJson>> getAllCategories();
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -1,10 +1,16 @@
 package guru.qa.niffler.api;
 
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import lombok.SneakyThrows;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+import java.util.List;
 
 public class SpendApiClient {
 
@@ -17,10 +23,108 @@ public class SpendApiClient {
 
   private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  @SneakyThrows
-  public SpendJson addSpend(SpendJson spendJson) {
-    return spendApi.addSpend(spendJson)
-        .execute()
-        .body();
+  public SpendJson addSpend(SpendJson spend) {
+    try {
+      Response<SpendJson> response = spendApi.addSpend(spend).execute();
+      if (!response.isSuccessful()) {
+        throw new ApiException("Failed to create spend. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while creating spend", e);
+    }
+  }
+
+  public SpendJson editSpend(SpendJson spend) {
+    try {
+      Response<SpendJson> response = spendApi.editSpend(spend).execute();
+      if (response.code() != 200) {
+        throw new ApiException("Failed to edit spend. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while editing spend", e);
+    }
+  }
+
+  public SpendJson getSpendById(String id) {
+    try {
+      Response<SpendJson> response = spendApi.getSpendById(id).execute();
+      if (response.code() != 200) {
+        throw new ApiException("Spend not found. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while fetching spend", e);
+    }
+  }
+
+  public List<SpendJson> getAllSpends(List<String> ids, CurrencyValues currencyFilter) {
+    try {
+      Response<List<SpendJson>> response = spendApi.getAllSpends(ids, currencyFilter).execute();
+      if (response.code() != 200) {
+        throw new ApiException("Failed to get spends. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while fetching spends", e);
+    }
+  }
+
+  public void deleteSpend(String id) {
+    try {
+      Response<Void> response = spendApi.removeSpend(id).execute();
+      if (response.code() != 200) {
+        throw new ApiException("Failed to delete spend. Code: " + response.code());
+      }
+    } catch (IOException e) {
+      throw new ApiException("Network error while deleting spend", e);
+    }
+  }
+
+  public CategoryJson createCategory(CategoryJson category) {
+    try {
+      Response<CategoryJson> response = spendApi.addCategory(category).execute();
+      if (response.code() != 200) {
+        throw new ApiException("Failed to create category. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while creating category", e);
+    }
+  }
+
+  public CategoryJson updateCategory(CategoryJson category) {
+    try {
+      Response<CategoryJson> response = spendApi.updateCategory(category).execute();
+      if (response.code() != 200) {
+        throw new ApiException("Failed to update category. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while updating category", e);
+    }
+  }
+
+  public List<CategoryJson> getAllCategories() {
+    try {
+      Response<List<CategoryJson>> response = spendApi.getAllCategories().execute();
+      if (response.code() != 200) {
+        throw new ApiException("Failed to get categories. Code: " + response.code());
+      }
+      return response.body();
+    } catch (IOException e) {
+      throw new ApiException("Network error while fetching categories", e);
+    }
+  }
+
+  public static class ApiException extends RuntimeException {
+    public ApiException(String message) {
+      super(message);
+    }
+
+    public ApiException(String message, Throwable cause) {
+      super(message, cause);
+    }
   }
 }
