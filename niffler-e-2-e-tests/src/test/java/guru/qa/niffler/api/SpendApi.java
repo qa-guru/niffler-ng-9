02ff6b19a1839866022b1aebda9f410365763738
Roman Katkov <r.katkov@mellow.io>
2025-06-22 17:09:56 +0700

    New requests added to SpendApi class

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -1,12 +1,36 @@
 package guru.qa.niffler.api;
 
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.DeleteSpendingJson;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface SpendApi {
 
   @POST("internal/spends/add")
   Call<SpendJson> addSpend(@Body SpendJson spending);
+
+  @PATCH("internal/spends/edit")
+  Call<SpendJson> editSpend(@Body SpendJson spending);
+
+  @GET("internal/spends/{id}")
+  Call<SpendJson> getSpendById(@Path("id") int userId);
+
+  @GET("internal/spends/all")
+  Call<List<SpendJson>> getSpendList();
+
+  @DELETE("internal/spends/remove")
+  Call<DeleteSpendingJson> deleteSpend();
+
+  @POST("internal/categories/add")
+  Call<CategoryJson> addCategory(@Body CategoryJson category);
+
+  @PATCH("internal/categories/update")
+  Call<CategoryJson> editCategory(@Body CategoryJson category);
+
+  @GET("internal/categories/all")
+  Call<List<CategoryJson>> getCategoriesList();
 }
