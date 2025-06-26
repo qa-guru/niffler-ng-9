dbdb8bc38dc7843b845e3c75f58e9b44b2567b9c
Roman Katkov <romangadle@gmail.com>
2025-06-26 23:20:38 +0700

    New annotations used with tests

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/ProfileTest.java
@@ -3,10 +3,16 @@ package guru.qa.niffler.test;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.DisabledByIssue;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension.StaticUser;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(UsersQueueExtension.class)
 public class ProfileTest {
 
     private static final Config CFG = Config.getInstance();
@@ -15,6 +21,7 @@ public class ProfileTest {
             username = "test1",
             archived = true
     )
+    @DisabledByIssue("3")
     @Test
     void archivedCategoryShouldPresentInCategoriesList(CategoryJson categoryJson){
         Selenide.open(CFG.frontUrl(), LoginPage.class)
@@ -27,13 +34,13 @@ public class ProfileTest {
     }
 
     @Category(
-            username = "test1",
+            username = "test2",
             archived = false
     )
     @Test
-    void activeCategoryShouldPresentInCategoriesList(CategoryJson categoryJson){
+    void activeCategoryShouldPresentInCategoriesList(CategoryJson categoryJson, @UserType(isEmpty = true) StaticUser user){
         Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .fillLoginPage("test1", "secret")
+                .fillLoginPage(user.username(), user.password())
                 .submit()
                 .checkThatPageLoaded()
                 .openProfilePageFromHeader()
