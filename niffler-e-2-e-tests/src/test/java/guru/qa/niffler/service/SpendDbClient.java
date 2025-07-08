8065613e87513e3ca397abdee824dd6ba4b033ce
marina <18-9292@mail.ru>
2025-07-08 20:34:58 +0300

    lesson 5 from video lesson

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -1,7 +1,6 @@
 package guru.qa.niffler.service;
 
-import guru.qa.niffler.data.dao.CategoryDao;
-import guru.qa.niffler.data.dao.SpendDao;
+import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
 import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
@@ -13,53 +12,105 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static guru.qa.niffler.data.Databases.transaction;
+
 public class SpendDbClient {
 
-    private final SpendDao spendDao = new SpendDaoJdbc();
-    private final CategoryDao categoryDao = new CategoryDaoJdbc();
+    private static final Config CFG = Config.getInstance();
 
     public SpendJson createSpend(SpendJson spend) {
-        SpendEntity spendEntity = SpendEntity.fromJson(spend);
-        if (spendEntity.getCategory().getId() == null) {
-            CategoryEntity categoryEntity = categoryDao.create(spendEntity.getCategory());
-            spendEntity.setCategory(categoryEntity);
-        }
-        return SpendJson.fromEntity(
-                spendDao.create(spendEntity)
+        return transaction(connection -> {
+                    SpendEntity spendEntity = SpendEntity.fromJson(spend);
+                    if (spendEntity.getCategory().getId() == null) {
+                        CategoryEntity categoryEntity = new CategoryDaoJdbc(connection)
+                                .create(spendEntity.getCategory());
+                        spendEntity.setCategory(categoryEntity);
+                    }
+                    return SpendJson.fromEntity(
+                            new SpendDaoJdbc(connection).create(spendEntity)
+                    );
+                },
+                CFG.spendJdbcUrl()
         );
     }
 
     public Optional<SpendEntity> findSpendById(UUID id) {
-        return spendDao.findSpendById(id);
+        return transaction(connection -> {
+                    Optional<SpendEntity> spend = new SpendDaoJdbc(connection)
+                            .findSpendById(id);
+                    return spend;
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public List<SpendEntity> findAllByUsername(String username) {
-        return spendDao.findAllByUsername(username);
+        return transaction(connection -> {
+                    List<SpendEntity> spend = new SpendDaoJdbc(connection)
+                            .findAllByUsername(username);
+                    return spend;
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public Boolean deleteSpend(UUID id) {
-        return spendDao.deleteSpend(id);
+        return transaction(connection -> {
+                    Boolean spend = new SpendDaoJdbc(connection)
+                            .deleteSpend(id);
+                    return spend;
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public CategoryEntity createCategory(CategoryJson categoryJson) {
-        CategoryEntity categoryEntity = CategoryEntity.fromJson(categoryJson);
-        return categoryDao.create(categoryEntity);
+        return transaction(connection -> {
+                    CategoryEntity categoryEntity = CategoryEntity.fromJson(categoryJson);
+                    return new CategoryDaoJdbc(connection)
+                            .create(categoryEntity);
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public List<CategoryEntity> findAllByUserName(String username) {
-        return categoryDao.findAllByUserName(username);
+        return transaction(connection -> {
+                    List<CategoryEntity> categories = new CategoryDaoJdbc(connection)
+                            .findAllByUserName(username);
+                    return categories;
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public Optional<CategoryEntity> findCategoryByUsernameAndCategoryName(String username, String categoryName) {
-        return categoryDao.findCategoryByUsernameAndCategoryName(username, categoryName);
+        return transaction(connection -> {
+                    Optional<CategoryEntity> categories = new CategoryDaoJdbc(connection)
+                            .findCategoryByUsernameAndCategoryName(username, categoryName);
+                    return categories;
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public Boolean updateCategory(CategoryJson category) {
-        CategoryEntity categoryEntity = CategoryEntity.fromJson(category);
-        return categoryDao.update(categoryEntity);
+        return transaction(connection -> {
+                    CategoryEntity categoryEntity = CategoryEntity.fromJson(category);
+                    return new CategoryDaoJdbc(connection)
+                            .update(categoryEntity);
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 
     public Boolean deleteCategory(UUID id) {
-        return categoryDao.delete(id);
+        return transaction(connection -> {
+                    Boolean categories = new CategoryDaoJdbc(connection)
+                            .delete(id);
+                    return categories;
+                },
+                CFG.spendJdbcUrl()
+        );
     }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
@@ -4,16 +4,45 @@ import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
 import guru.qa.niffler.data.entity.userdata.UserEntity;
 import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
+import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.service.SpendDbClient;
 import guru.qa.niffler.service.UserDbClient;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Disabled
 public class JdbcTest {
+
+    @Test
+    void txTest() {
+        SpendDbClient spendDbClient = new SpendDbClient();
+
+        SpendJson spend = spendDbClient.createSpend(
+                new SpendJson(
+                        null,
+                        new Date(),
+                        new CategoryJson(
+                                null,
+                                "cat-name-tx-2",
+                                "duck",
+                                false
+                        ),
+                        CurrencyValues.RUB,
+                        1000.0,
+                        "spend-name-tx",
+                        null
+                )
+        );
+
+        System.out.println(spend);
+    }
 
     @Test
     void checkCategoryUpdate(){
