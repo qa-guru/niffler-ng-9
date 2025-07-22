5da8ab1e242616d9c3151207f9b42336c7ed8e6c
dtuchs <dtuchs@gmail.com>
2025-07-22 19:26:58 +0300

    Homeworks for streams #5.1-5.2 - method findAll() in DAOs, RowMappers for Category & Authority, handle EmptyResultDataAccessException in Spring-jdbc DAOs, small refactoring

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
@@ -2,6 +2,10 @@ package guru.qa.niffler.data.dao;
 
 import guru.qa.niffler.data.entity.auth.AuthorityEntity;
 
+import java.util.List;
+
 public interface AuthAuthorityDao {
   void create(AuthorityEntity... authority);
+
+  List<AuthorityEntity> findAll();
 }
