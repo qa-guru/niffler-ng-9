9d5b70ab526b67d7cc57b8e2e6e797e6f34880d4
dtuchs <dtuchs@gmail.com>
2025-08-19 21:00:57 +0700

    Video #9.2 [12.08.25] - Allure - custom template for SQL queries

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/RestClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/RestClient.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.service;
 
 import guru.qa.niffler.api.core.ThreadSafeCookieStore;
 import guru.qa.niffler.config.Config;
+import io.qameta.allure.okhttp3.AllureOkHttp3;
 import okhttp3.Interceptor;
 import okhttp3.JavaNetCookieJar;
 import okhttp3.OkHttpClient;
@@ -58,13 +59,13 @@ public abstract class RestClient {
 
     clientBuilder
         .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(level))
+        .addNetworkInterceptor(new AllureOkHttp3())
         .cookieJar(
             new JavaNetCookieJar(
                 new CookieManager(
                     ThreadSafeCookieStore.INSTANCE,
                     CookiePolicy.ACCEPT_ALL
                 )
-
             )
         );
 
