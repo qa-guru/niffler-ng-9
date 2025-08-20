fb10858699507335303208cfe8fab6048e87d6d0
dboyko <darya.boyko@gmail.com>
2025-08-20 18:55:45 +0300

    6.1 - jpa. update formatting

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/repository/UserdataUserRepository.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/repository/UserdataUserRepository.java
@@ -6,9 +6,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserdataUserRepository {
-    UserEntity create (UserEntity user);
+    UserEntity create(UserEntity user);
+
     Optional<UserEntity> findById(UUID id);
-    void addIncomeInvitation (UserEntity requester, UserEntity addressee);
+
+    void addIncomeInvitation(UserEntity requester, UserEntity addressee);
+
     void addOutcomeInvitation(UserEntity requester, UserEntity addressee);
+
     void addFriend(UserEntity requester, UserEntity addressee);
 }
